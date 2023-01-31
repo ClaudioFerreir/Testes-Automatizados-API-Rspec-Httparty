21569c8c2e15d49169dba7624e7b189d6a20bef6
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-01-31 15:05:06 -0300

    tag spec lesson

---
+++ RSPEC/spec/tag_spec.rb
@@ -0,0 +1,20 @@
+describe 'trabalhando', teste_tag: true do 
+  it 'com tag' do
+    puts 'usei a tag com nome teste_tag'
+  end
+end
+
+describe 'andando', :teste_simbolo do 
+  it 'com tag um' do
+    puts 'usei a tag com nome teste_simbolo'
+  end
+end
+
+#nao quero que rode esta tag
+#adicionamos no arquivo .rspec a linha '--tag ~nao_teste_tag'
+
+describe 'correndo', :nao_teste_tag do 
+  it 'com tag dois' do
+    puts 'usei a tag com nome nao_teste_tag'
+  end
+end
