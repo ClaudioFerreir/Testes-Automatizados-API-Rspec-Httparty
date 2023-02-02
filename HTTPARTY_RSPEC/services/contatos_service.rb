ea22032cadbf4aa7ff6f285d46ec89501c5dd1b5
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-02-02 15:03:21 -0300

    config lesson

---
+++ HTTPARTY_RSPEC/services/contatos_service.rb
@@ -0,0 +1,13 @@
+module Contato
+  include HTTParty
+  #url base de contato
+  base_uri 'http://localhost:3000'
+  #opcoes do meu service
+  format :json
+  headers Accept: 'application/vnd.tasksmanager.v2',
+          'Content-Type': 'application/json'
+end
+
+# toda vez que eu chamar este modulo a url padrao dele sera 'https://api-de-tarefas.herokuapp.com'
+# e ele tera o headers com a opcoes Accept: 'application/vdn.tasksmanager.v2', 'Content-Type': 'application/json'
+# e sera do formato json
