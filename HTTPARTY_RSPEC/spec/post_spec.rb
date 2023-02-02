2f6727c87200edf9f67a488878798e9fb96c74aa
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-02-02 15:03:39 -0300

    post lesson

---
+++ HTTPARTY_RSPEC/spec/post_spec.rb
@@ -0,0 +1,26 @@
+describe 'fazer um requisição' do
+  it 'post' do
+    @body = {
+      "name": "bruno4",
+      "last_name": "batista4",
+      "email": "bruno4@gmail.com",
+      "age": 28,
+      "phone": 21984759575,
+      "address": "Rua dois",
+      "state": "Minas Gerais",
+      "city": "Belo Horizonte"
+    }.to_json
+
+    # forma simples do httparty
+    #@headers = {
+    #  'Content-Type': 'application/json',
+    #  'Accept': 'application/vnd.tasksmanager.v2'
+    #}
+
+    #@request = HTTParty.post('http://localhost:3000/contacts', body: @body, headers: @headers)
+    #puts @request
+
+    @request = Contato.post('/contacts', body: @body)
+    puts @request
+  end
+end
