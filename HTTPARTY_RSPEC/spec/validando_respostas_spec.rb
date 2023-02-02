eb052921ea0eb6d0353e2a0c66066b67cbfaee40
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-02-02 17:15:20 -0300

    added coment lesson

--- HTTPARTY_RSPEC/spec/validando_respostas_spec.rb
+++ HTTPARTY_RSPEC/spec/validando_respostas_spec.rb
@@ -4,7 +4,7 @@ describe 'fazer uma requisição' do
     #puts @melhor_opcao
     #puts @melhor_opcao.code #retorna o codigo de validacao ex. 200 ok
     #expect(@melhor_opcao.code).to eq 200
-    puts @melhor_opcao.body #observar o formato de retorno, string ou json.
+    puts @melhor_opcao.body #observar o formato de retorno, string ou json. Caso esta chamada venha um json aplicamos direto na chamada a busca pelo elemento.
     #puts @melhor_opcao.parsed_response['data']['attributes']['name'] #usamos o metodo 'parsed_response' para converter uma string em um json
     expect(@melhor_opcao.parsed_response['data']['attributes']['name']).to eq 'bruno 1'
     expect(@melhor_opcao.parsed_response['data']['attributes']['last-name']).to eq 'batista 1'
